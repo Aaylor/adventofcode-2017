f7a0faa459ffb8b533f334e3ca835ecaafd16e82
Loïc Runarvot <loic.runarvot@trust-in-soft.com>
2017-12-11 19:38:10 +0100

    day09/problem01

---
+++ day09/problem01/group.ml
@@ -0,0 +1,51 @@
+
+let eval stream =
+  let rec eval_group depth sum =
+    match Stream.next stream with
+    | '{' -> eval_group (succ depth) sum
+    | ',' -> eval_group depth sum
+    | '}' ->
+      (* We assume that the input always start with '{'. Then, closing
+         the first bracket would lead to depth equal to 0, which is
+         the end of input.
+         We make the assertion that the input cannot contain mutliple
+         "toplevel" groups. *)
+      if depth = 1 then succ sum
+      else eval_group (pred depth) (sum + depth)
+    | '<' ->
+      garbage depth sum
+    | _ ->
+      failwith "Malformed input"
+  and garbage depth sum =
+    match Stream.next stream with
+    | '>' ->
+      eval_group depth sum
+    | '!' ->
+      ignore (Stream.next stream);
+      garbage depth sum
+    | _ ->
+      garbage depth sum
+  in
+  eval_group 0 0
+
+
+(* INPUT *)
+
+let with_channel filename fn =
+  let channel = open_in filename in
+  try
+    let result = fn channel in
+    close_in channel;
+    result
+  with exn ->
+    close_in channel;
+    raise exn
+
+let () =
+  let result =
+    with_channel "input"
+      (fun channel ->
+         let stream = Stream.of_channel channel in
+         eval stream)
+  in
+  Format.printf "%d@." result
