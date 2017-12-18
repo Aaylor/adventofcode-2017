ee17aafe5d4b86c901063d451f6a97209313f5b4
Loïc Runarvot <loic.runarvot@trust-in-soft.com>
2017-12-18 11:38:43 +0100

    day18

--- day18/problem02/duet.ml
+++ day18/problem02/duet.ml
@@ -8,7 +8,7 @@ type rvalue =
   | Int of int
 
 type instruction =
-  | Sound of rvalue
+  | Send of rvalue
   | Set of register * rvalue
   | Add of register * rvalue
   | Mul of register * rvalue
@@ -46,40 +46,70 @@ let eval_operation context op register rvalue =
   let rvalue_value = eval_rvalue context rvalue in
   Register.add register (op register_value rvalue_value) context
 
-let do_duet instructions =
-  let rec aux_duet ~last_sound context index =
+let start_evaluation ~pid instructions command =
+  let rec aux_duet ~stack ~queue context index =
     match instructions.(index) with
-    | Sound register ->
-      let register_value = eval_rvalue context register in
-      aux_duet ~last_sound:(Some register_value) context (succ index)
+    | Send rvalue ->
+      let register_value = eval_rvalue context rvalue in
+      aux_duet ~stack:(register_value :: stack) ~queue context (succ index)
     | Set (register, rvalue) ->
       let rvalue = eval_rvalue context rvalue in
       let context' = Register.add register rvalue context in
-      aux_duet ~last_sound context' (succ index)
+      aux_duet ~stack ~queue context' (succ index)
     | Add (register, rvalue) ->
       let context' = eval_operation context ( + ) register rvalue in
-      aux_duet ~last_sound context' (succ index)
+      aux_duet ~stack ~queue context' (succ index)
     | Mul (register, rvalue) ->
       let context' = eval_operation context ( * ) register rvalue in
-      aux_duet ~last_sound context' (succ index)
+      aux_duet ~stack ~queue context' (succ index)
     | Mod (register, rvalue) ->
       let context' = eval_operation context ( mod ) register rvalue in
-      aux_duet ~last_sound context' (succ index)
+      aux_duet ~stack ~queue context' (succ index)
     | Recover register ->
-      let register_value = get_register_value context register in
-      if register_value <> 0 then last_sound
-      else aux_duet ~last_sound context (succ index)
+      begin
+        match queue with
+        | [] ->
+          `Waiting (index, context, List.rev stack)
+        | x :: xs ->
+          let context' = Register.add register x context in
+          aux_duet ~stack ~queue:xs context' (succ index)
+      end
     | Jump (register, rvalue) ->
       let register_value = eval_rvalue context register in
       let index =
         if register_value > 0 then index + (eval_rvalue context rvalue)
         else succ index
       in
-      aux_duet ~last_sound context index
+      aux_duet ~stack ~queue context index
   in
-  match aux_duet ~last_sound:None Register.empty 0 with
-  | None -> failwith "no sound played"
-  | Some sound -> sound
+  match command with
+  | `Starting ->
+    let context = Register.singleton "p" pid in
+    aux_duet ~stack:[] ~queue:[] context 0
+  | `Resume (index, context, queue) ->
+    aux_duet ~stack:[] ~queue context index
+
+let rec eval_duet ~sent instructions p1 p2 =
+  match p1, p2 with
+  | `Waiting (p1_index, p1_context, []),
+    `Waiting (p2_index, p2_context, []) ->
+    sent
+  | `Waiting (p1_index, p1_context, p1_stack),
+    `Waiting (p2_index, p2_context, p2_stack) ->
+    let sent = sent + List.length p2_stack in
+    (* Creates command to resume both programs *)
+    let p1_command = `Resume (p1_index, p1_context, p2_stack) in
+    let p2_command = `Resume (p2_index, p2_context, p1_stack) in
+    (* Then restart them *)
+    let p1' = start_evaluation ~pid:0 instructions p1_command in
+    let p2' = start_evaluation ~pid:1 instructions p2_command in
+    (* Eval the result again *)
+    eval_duet ~sent instructions p1' p2'
+
+let do_duet instructions =
+  let p1 = start_evaluation ~pid:0 instructions `Starting in
+  let p2 = start_evaluation ~pid:1 instructions `Starting in
+  eval_duet ~sent:0 instructions p1 p2
 
 
 (* INPUT *)
@@ -101,7 +131,7 @@ let parse_instruction line =
   in
   match String.split_on_char ' ' line with
   | [ "snd"; rvalue ] ->
-    Sound (parse_rvalue rvalue)
+    Send (parse_rvalue rvalue)
   | [ "rcv"; register ] ->
     Recover register
   | [ "set"; register; rvalue ] ->
